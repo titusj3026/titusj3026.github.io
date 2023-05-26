f503a2e2b1b3988555d33bcca170e91c3171e16d
titus <64428706+titusj3026@users.noreply.github.com>
2023-05-25 22:33:25 -0600

    Update shell.js

--- js/shell.js
+++ js/shell.js
@@ -7,7 +7,7 @@ const welcome = `<div class="nomobile">
    / \\<span data-color="orange">__</span>/<span data-color="orange">----</span>\\/   \\     <span data-color="lime">  / /  / _ \\ / ___// __ \`__ \\ / /_/ // __ \\ / ___// __/</span>
   /<span data-color="orange">---</span>\\/      \\    \\    <span data-color="lime"> / /  /  __// /   / / / / / // ____// /_/ // /   / /_</span>
  /     \\       \\    \\   <span data-color="lime">/_/   \\___//_/   /_/ /_/ /_//_/     \\____//_/    \\__/</span>
-<span data-color="cyan"><~~~~~~~~~~~~~~~~~~~~></span>        <span data-color="grey">===> Terminal Portfolio | Made By <a href="https://new.sx9.is-a.dev">sx9dev</a></span></div>
+<span data-color="cyan"><~~~~~~~~~~~~~~~~~~~~></span>        <span data-color="grey">===> Terminal Portfolio </span></div>
 
 Type <span data-color="yellow">help</span> For List Of Available Commands
 
